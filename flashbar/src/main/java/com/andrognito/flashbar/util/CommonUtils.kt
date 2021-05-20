3ba3e2a7704adeedbf82b568aedd5bcaecf2acc5
Zhiwei Deng <zhiwei.deng@500px.com>
2021-05-20 16:44:17 +0800

    Activity rootView use contentView

--- flashbar/src/main/java/com/andrognito/flashbar/util/CommonUtils.kt
+++ flashbar/src/main/java/com/andrognito/flashbar/util/CommonUtils.kt
@@ -50,7 +50,7 @@ internal fun Activity?.getRootView(): ViewGroup? {
     if (this == null || window == null || window.decorView == null) {
         return null
     }
-    return window.decorView as ViewGroup
+    return window.decorView.findViewById(android.R.id.content) as? ViewGroup
 }
 
 internal fun Context.convertDpToPx(dp: Int): Int {
